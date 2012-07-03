a60efd07a0a897112bbb6f1f480d13892c623653
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-07-03 21:28:55 +0200

    Create method for AJAX request

--- public/app.js
+++ public/app.js
@@ -1,12 +1,19 @@
+function addStatus(options) {
+    $.ajax({
+        url: '/statuses',
+        type: 'POST',
+        dataType: 'json',
+        data: { text: options.text },
+        success: options.success
+    });
+}
+
 jQuery(function() {
     $("#new-status").submit(function(e) {
         e.preventDefault();
 
-        $.ajax({
-            url: '/statuses',
-            type: 'POST',
-            dataType: 'json',
-            data: { text: $(this).find('textarea').val() },
+        addStatus({
+            text: $(this).find('textarea').val(),
             success: function(data) {
                 $("#statuses").append('<li>' + data.text + '</li>');
                 $("#new-status textarea").val("");
