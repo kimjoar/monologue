f9f64b817e59abc870bd7bcd47511ec5979b7e65
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-07-09 18:07:40 +0200

    Ready for presentation

--- public/app.js
+++ public/app.js
@@ -1,56 +1,16 @@
-var Status = Simple.Model.extend({
-    url: '/status'
-});
+$(document).ready(function() {
+    $('#new-status form').submit(function(e) {
+        e.preventDefault();
 
-var Statuses = Simple.Model.extend({
-    add: function(text) {
-        var status = new Status({ text: text });
-        var that = this;
-        status.save({
+        $.ajax({
+            url: '/status',
+            type: 'POST',
+            dataType: 'json',
+            data: { text: $('#new-status textarea').val() },
             success: function(data) {
-                that.trigger("added", data.text);
+                $('#statuses ul').append('<li>' + data.text + '</li>');
+                $('#new-status textarea').val('');
             }
         });
-    }
-});
-
-var NewStatusView = Simple.View.extend({
-    events: {
-        "submit": "addStatus"
-    },
-
-    initialize: function(options) {
-        this.statuses = options.statuses;
-        this.statuses.on("added", this.reset, this);
-    },
-
-    addStatus: function(e) {
-        e.preventDefault();
-        this.statuses.add(this.text());
-    },
-
-    text: function() {
-        return this.DOM('textarea').val();
-    },
-
-    reset: function() {
-        this.DOM('textarea').val("");
-    }
-});
-
-var StatusesView = Simple.View.extend({
-    initialize: function(options) {
-        this.statuses = options.statuses;
-        this.statuses.on("added", this.appendStatus, this);
-    },
-
-    appendStatus: function(text) {
-        this.el.append('<li>' + text + '</li>');
-    }
-});
-
-jQuery(function() {
-    var statuses = new Statuses();
-    new NewStatusView({ el: $("#new-status"), statuses: statuses });
-    new StatusesView({ el: $("#statuses"), statuses: statuses });
+    });
 });
