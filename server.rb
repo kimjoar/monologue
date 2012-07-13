1bedeec007d1ab473cda38b462cc9b9ad7cb865c
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-07-13 19:08:51 +0200

    Deliver app.js through Ruby

--- server.rb
+++ server.rb
@@ -5,6 +5,11 @@ get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
+get '/app.js' do
+  content_type 'application/javascript'
+  File.read(step(params[:step] || ""))
+end
+
 post '/status' do
   data = if params[:text]
            params
@@ -15,3 +20,19 @@ post '/status' do
   content_type :json
   data.to_json
 end
+
+def step(param)
+  if (match = param.match(/\A([0-9]{1,2})\z/))
+    step = match[1]
+  end
+
+  return step_file unless step
+  return step_file unless File.exists?(step_file(step))
+
+  step_file step
+end
+
+def step_file(step = 1)
+  File.join('public', "step#{step}.js")
+end
+
