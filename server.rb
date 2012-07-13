748c12041f0f1b2abb8b05ed281f40fb394786d9
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-07-13 21:19:55 +0200

    Refactor server to allow query params on main url

--- server.rb
+++ server.rb
@@ -1,13 +1,14 @@
 require 'sinatra'
 require 'json'
 
-get '/' do
-  File.read(File.join('public', 'index.html'))
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape
 end
 
-get '/app.js' do
-  content_type 'application/javascript'
-  File.read(step(params[:step] || ""))
+get '/' do
+  @step = step(params[:step] || "")
+  erb :index
 end
 
 post '/status' do
@@ -23,16 +24,8 @@ end
 
 def step(param)
   if (match = param.match(/\A([0-9]{1,2})\z/))
-    step = match[1]
+    match[1]
+  else
+    1
   end
-
-  return step_file unless step
-  return step_file unless File.exists?(step_file(step))
-
-  step_file step
 end
-
-def step_file(step = 1)
-  File.join('public', "step#{step}.js")
-end
-
