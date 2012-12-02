6aec8eacd2346958f00966345e77703adc94186b
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-12-02 19:44:24 +0100

    Introduce require.js

--- public/monologue.js
+++ public/monologue.js
@@ -1,43 +1,64 @@
-var Status = Backbone.Model.extend({
-    url: '/status'
-});
-
-var Statuses = Backbone.Collection.extend({
-    model: Status
-});
-
-var NewStatusView = Backbone.View.extend({
-    events: {
-        "submit form": "addStatus"
+requirejs.config({
+    paths: {
+        'jquery': 'vendor/jquery-1.8.3',
+        'underscore': 'vendor/underscore-1.4.2',
+        'backbone': 'vendor/backbone-0.9.2'
     },
-
-    initialize: function(options) {
-        this.collection.on("add", this.clearInput, this);
-    },
-
-    addStatus: function(e) {
-        e.preventDefault();
-
-        this.collection.create({ text: this.$('textarea').val() });
-    },
-
-    clearInput: function() {
-        this.$('textarea').val('');
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
     }
 });
 
-var StatusesView = Backbone.View.extend({
-    initialize: function(options) {
-        this.collection.on("add", this.appendStatus, this);
-    },
+require(['jquery', 'backbone'], function($, Backbone) {
 
-    appendStatus: function(status) {
-        this.$('ul').append('<li>' + status.escape("text") + '</li>');
-    }
-});
+    var Status = Backbone.Model.extend({
+        url: '/status'
+    });
 
-$(document).ready(function() {
-    var statuses = new Statuses();
-    new NewStatusView({ el: $('#new-status'), collection: statuses });
-    new StatusesView({ el: $('#statuses'), collection: statuses });
+    var Statuses = Backbone.Collection.extend({
+        model: Status
+    });
+
+    var NewStatusView = Backbone.View.extend({
+        events: {
+            "submit form": "addStatus"
+        },
+
+        initialize: function(options) {
+            this.collection.on("add", this.clearInput, this);
+        },
+
+        addStatus: function(e) {
+            e.preventDefault();
+
+            this.collection.create({ text: this.$('textarea').val() });
+        },
+
+        clearInput: function() {
+            this.$('textarea').val('');
+        }
+    });
+
+    var StatusesView = Backbone.View.extend({
+        initialize: function(options) {
+            this.collection.on("add", this.appendStatus, this);
+        },
+
+        appendStatus: function(status) {
+            this.$('ul').append('<li>' + status.escape("text") + '</li>');
+        }
+    });
+
+    $(document).ready(function() {
+        var statuses = new Statuses();
+        new NewStatusView({ el: $('#new-status'), collection: statuses });
+        new StatusesView({ el: $('#statuses'), collection: statuses });
+    });
+
 });
